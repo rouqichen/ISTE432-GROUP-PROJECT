f10e444c7e9858efbb179442a0502a062c424fed
ROUQI <71988428+rouqichen@users.noreply.github.com>
2021-05-01 14:33:05 -0400

    Update index.js

--- FrontEnd/routes/index.js
+++ FrontEnd/routes/index.js
@@ -1,9 +1,35 @@
 var express = require('express');
 var router = express.Router();
+router.use(express.urlencoded());
+router.use(express.json());
+
+
+const axios = require('axios');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.post('/login', function(req, res, next) {
+  var username = req.body.name;
+  var password = req.body.password;
+  console.log(username);
+
+  axios.post('http://localhost:8080/login' + userToken).then(function (backendResponse) {
+    console.log(username);
+});
+});
+
+router.post('/register', function(req, res, next) {
+  var username = req.body.name;
+  var password = req.body.password;
+
+  axios.post('http://localhost:8080/register' + userToken).then(function (backendResponse) {
+      console.log(username);
+  });
+});
+
+
 
 module.exports = router;
